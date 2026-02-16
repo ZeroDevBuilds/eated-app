696f2f44ebe0495609f37cc7f608036411561f60
zerodevbuilds <zerodevbuilds@gmail.com>
2026-02-16 21:18:07 +0100

    fix keyboard overlay issue

--- app/src/main/java/com/zerodevbuilds/eated/ui/screen/batchdish/BatchDishScreen.kt
+++ app/src/main/java/com/zerodevbuilds/eated/ui/screen/batchdish/BatchDishScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -77,6 +78,7 @@ fun BatchDishScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
+                .imePadding()
         ) {
             LazyColumn(
                 modifier = Modifier.weight(1f),
